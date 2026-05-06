673c5e0bdf73bce1a4fb353b97dadb1a9ee5f34b
techbydeepak <deepakrathour05tech@gmail.com>
2026-05-06 12:20:34 +0530

    new tech current fix

--- src/app/api/current/route.js
+++ src/app/api/current/route.js
@@ -1,17 +1,54 @@
 import { NextResponse } from "next/server";
-import jwt from "jsonwebtoken"
-import { User } from "@/models/user"; 
-
+import jwt from "jsonwebtoken";
+import { User } from "@/models/user";
+import { connectDb } from "@/helper/db";
 
 export async function GET(request) {
-   
-   const authToken = request.cookies.get("authToken")?.value
-   console.log(authToken);
 
-   const data = jwt.verify(authToken, process.env.JWT_KEY);
-   console.log(data);
+  try {
 
- const user =  await User.findById(data._id).select("-password");
-   
-   return NextResponse.json({user})
+    const authToken = request.cookies.get("authToken")?.value;
+
+    console.log(authToken);
+
+    // IMPORTANT FIX
+    if (!authToken) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "No token found",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    await connectDb();
+
+    const data = jwt.verify(authToken, process.env.JWT_KEY);
+
+    console.log(data);
+
+    const user = await User.findById(data._id).select("-password");
+
+    return NextResponse.json({
+      success: true,
+      user,
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Invalid token",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
 }
