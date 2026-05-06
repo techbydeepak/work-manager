54da179057680b535e78a661d1cb3529ea05a9ed
techbydeepak <deepakrathour05tech@gmail.com>
2026-05-06 13:41:12 +0530

    Fix production middleware authhh

--- src/app/api/current/route.js
+++ src/app/api/current/route.js
@@ -1,54 +1,17 @@
 import { NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
-import { User } from "@/models/user";
-import { connectDb } from "@/helper/db";
+import jwt from "jsonwebtoken"
+import { User } from "@/models/user"; 
+
 
 export async function GET(request) {
+   
+   const authToken = request.cookies.get("authToken")?.value
+   console.log(authToken);
 
-  try {
+   const data = jwt.verify(authToken, process.env.JWT_KEY);
+   console.log(data);
 
-    const authToken = request.cookies.get("authToken")?.value;
-
-    console.log(authToken);
-
-    // IMPORTANT FIX
-    if (!authToken) {
-      return NextResponse.json(
-        {
-          success: false,
-          message: "No token found",
-        },
-        {
-          status: 401,
-        }
-      );
-    }
-
-    await connectDb();
-
-    const data = jwt.verify(authToken, process.env.JWT_KEY);
-
-    console.log(data);
-
-    const user = await User.findById(data._id).select("-password");
-
-    return NextResponse.json({
-      success: true,
-      user,
-    });
-
-  } catch (error) {
-
-    console.log(error);
-
-    return NextResponse.json(
-      {
-        success: false,
-        message: "Invalid token",
-      },
-      {
-        status: 401,
-      }
-    );
-  }
+ const user =  await User.findById(data._id).select("-password");
+   
+   return NextResponse.json({user})
 }
